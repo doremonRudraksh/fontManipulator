afd9b06e7d4375d42370ac3363725fa6e3039053
doremonRudraksh <90757183+doremonRudraksh@users.noreply.github.com>
2022-02-03 16:23:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+left_wrist_x = 0;
+right_wrist_x = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(400,400);
@@ -6,15 +10,19 @@ function setup(){
     canvas = createCanvas(800,400);
     canvas.position(430,120);
 
-    poseNet = ml5.poseNet(video,modelDone);
+    poseNet = ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotposes);
 }
 
 function draw(){
     background("#5196e3");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#00ff0a");
+    text('Rudraksh Sharma',50,400);
 }
 
-function modelDone(){
+function modelLoaded(){
     console.log("PoseNet Is Initialized And Loaded");
 }
 
@@ -24,6 +32,11 @@ function gotposes(results,error){
     }
     if(results.length > 0){
         console.log(results);
+
+        left_wrist_x = results[0].pose.leftWrist.x;
+        right_wrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(left_wrist_x - right_wrist_x)
 
         console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
         console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
